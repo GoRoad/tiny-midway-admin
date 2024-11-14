142b5bc80f904343949a6a27dbd92d0391760be0
34892002@qq.com <34892002@qq.com>
2024-11-14 11:51:13 +0800

    fix: 报错信息优化

--- server/src/filter/default.filter.ts
+++ server/src/filter/default.filter.ts
@@ -5,17 +5,21 @@ import { Context } from '@midwayjs/koa';
 export class DefaultErrorFilter {
   async catch(err: any, ctx: Context) {
     // 所有的未分类错误会到这里
-    const code = +(err.code || err.status || 500);
+    const code = err.status || err.code;
+    // 把code转换成数字类型
+    const code2 = +code;
     const msg = err.message;
     console.log('@统一异常捕获处理: ', code, msg);
-    // 不拦截下面的code，直接抛出http状态码让前端处理
-    const ignore = [401, 403];
-    if (ignore.includes(code)) {
-      ctx.status = code;
-      ctx.body = err;
+    // 不拦截下面的code，前端有特殊处理
+    const ignore = [401, 403, 500];
+    if (ignore.includes(code2)) {
+      ctx.status = code2;
+      ctx.body = { message: msg };
     } else {
-      // 前端会弹出所有code不为0或者200的msg信息
-      return { code, error: 'DefaultErrorFilter', message: msg };
+      // 抛出异常信息
+      // 前端抛出code不为0或者200的msg信息弹窗
+      // 没有具体业务code的错误统一返回1000
+      return { code: 1000, error: 'DefaultError', message: msg };
     }
   }
 }
