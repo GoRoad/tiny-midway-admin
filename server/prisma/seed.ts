bd3384f6c92f153313b9a275379876bb201bc585
34892002@qq.com <34892002@qq.com>
2024-12-06 10:53:47 +0800

    feat: 兼容postgresql

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 const prisma = new PrismaClient();
 const main = async () => {
@@ -55,96 +55,509 @@ const main = async () => {
     });
   })
 
-  /**
-   * 插入数据到数据库中
-   *
-   * @param table 要插入数据的表
-   * @param keys 要插入数据的字段名称数组
-   * @param values 要插入的数据值数组，数组中的元素顺序与 keys 数组中的字段名称顺序相对应
-   * @param whereIndex 指定哪个字段作为查询条件，用于在插入后获取刚插入的数据。默认为 -1，表示不查询刚插入的数据
-   * @returns 如果 whereIndex 大于 -1，则返回刚插入的数据；否则返回 true
-   */
-  const insertSql = async (table: string, keys: string[], values: any[], whereIndex: number = -1) => {
-    // 生成插入语句
-    const insertQuery = `
-      INSERT INTO "${table}" (${keys.map(key => `"${key}"`).join(', ')})
-      VALUES (${values.map((_, i) => `$${i + 1}`).join(', ')});
-    `;
-
-    // 执行插入操作
-    await prisma.$executeRawUnsafe(insertQuery, ...values);
-
-    if (whereIndex > -1) {
-      // 返回刚插入的数据
-      return await prisma.resource.findFirst({
-        where: {
-          name: values[whereIndex]
-        }
-      });
-    } else {
-      return { id: null };
-    }
+  const insertResource = async (data: Prisma.ResourceCreateInput) => {
+    return await prisma.resource.create({ data });
   };
 
   // 插入资源数据
-  const resourceKeys = ["name", "code", "type", "parent_id", "path", "redirect", "icon", "component", "layout", "keepAlive", "method", "description", "show", "enable", "order", "createTime", "updateTime"];
-  // 基础功能
-  let parent = await insertSql('resource', resourceKeys, Array.of<any>('基础功能', 'Base', 'MENU', null, null, null, 'i-fe:grid', null, '', null, null, null, 1, 1, 0, 1729182565455, 1729182565455), 0) as any;
-  await insertSql('resource', resourceKeys, Array.of<any>('图标 Icon', 'Icon', 'MENU', parent.id, '/base/icon', null, 'i-fe:feather', '/src/views/base/unocss-icon.vue', '', null, null, null, 1, 1, 0, 1729182631376, 1729182631376));
-  await insertSql('resource', resourceKeys, Array.of<any>('基础组件', 'BaseComponents', 'MENU', parent.id, '/base/components', null, 'i-me:awesome', '/src/views/base/index.vue', '', null, null, null, 1, 1, 1, 1729182694990, 1729182694990));
-  await insertSql('resource', resourceKeys, Array.of<any>('Unocss', 'Unocss', 'MENU', parent.id, '/base/unocss', null, 'i-me:awesome', '/src/views/base/unocss.vue', '', null, null, null, 1, 1, 2, 1729182757663, 1729182757663));
-  await insertSql('resource', resourceKeys, Array.of<any>('KeepAlive', 'KeepAlive', 'MENU', parent.id, '/base/keep-alive', null, 'i-me:awesome', '/src/views/base/keep-alive.vue', '', 1, null, null, 1, 1, 3, 1729182816011, 1729182816011));
-  await insertSql('resource', resourceKeys, Array.of<any>('MeModal', 'TestModal', 'MENU', parent.id, '/testModal', null, 'i-me:dialog', '/src/views/base/test-modal.vue', '', null, null, null, 1, 1, 4, 1729182852055, 1729182852055));
-  // 业务示例Demo
-  parent = await insertSql('resource', resourceKeys, Array.of<any>('业务示例', 'Demo', 'MENU', null, null, null, 'i-fe:grid', null, '', null, null, null, 1, 1, 1, 1729182885385, 1729182885385), 0);
-  await insertSql('resource', resourceKeys, Array.of<any>('图片上传', 'ImgUpload', 'MENU', parent.id, '/demo/upload', null, 'i-fe:image', '/src/views/demo/upload/index.vue', '', 1, null, null, 1, 1, 0, 1729182922104, 1729182922104));
-  let crudItem = await insertSql('resource', resourceKeys, Array.of<any>('CRUD表单', 'CrudDemo', 'MENU', parent.id, '/demo/crud', null, 'i-fe:database', '/src/views/demo/crud/index.vue', '', 1, null, null, 1, 1, 0, 1729182922104, 1729182922104), 0);
-  await insertSql('resource', resourceKeys, Array.of<any>('富文本', 'DemoRichText', 'MENU', parent.id, '/demo/rich-text', null, 'i-fe:database', '/src/views/demo/rich-text/index.vue', '', 1, null, null, 1, 1, 0, 1729182922104, 1729182922104));
-  // crud例子-权限按钮
-  await insertSql('resource', resourceKeys, Array.of<any>('添加', 'demo:crud:add', 'BUTTON', crudItem!.id, null, null, null, null, '', null, null, null, 1, 1, 0, 1729218654933, 1729218654933));
-  await insertSql('resource', resourceKeys, Array.of<any>('删除', 'demo:crud:remove', 'BUTTON', crudItem!.id, null, null, null, null, '', null, null, null, 1, 1, 0, 1729218654933, 1729218654933));
-  await insertSql('resource', resourceKeys, Array.of<any>('查看', 'demo:crud:view', 'BUTTON', crudItem!.id, null, null, null, null, '', null, null, null, 1, 1, 0, 1729218654933, 1729218654933));
-  await insertSql('resource', resourceKeys, Array.of<any>('编辑', 'demo:crud:edit', 'BUTTON', crudItem!.id, null, null, null, null, '', null, null, null, 1, 1, 0, 1729218654933, 1729218654933));
-  // 数据管理
-  parent = await insertSql('resource', resourceKeys, Array.of<any>('数据管理', 'Data', 'MENU', null, null, null, 'i-fe:grid', null, '', null, null, null, 1, 1, 1, 1729182885385, 1729182885385), 0);
-  await insertSql('resource', resourceKeys, Array.of<any>('数据字典', 'DataDict', 'MENU', parent.id, '/data/dict', null, 'i-fe:book', '/src/views/data/dict/index.vue', '', 1, null, null, 1, 1, 0, 1729182922104, 1729182922104));
-  await insertSql('resource', resourceKeys, Array.of<any>('文件管理', 'DataFile', 'MENU', parent.id, '/data/file', null, 'i-fe:file', '/src/views/data/file/index.vue', '', 1, null, null, 1, 1, 0, 1729182922104, 1729182922104));
-  // 系统管理
-  parent = await insertSql('resource', resourceKeys, Array.of<any>('系统管理', 'SysMgt', 'MENU', null, null, null, 'i-fe:grid', null, '', null, null, null, 1, 1, 2, 1729217819578, 1729217819578), 0);
-  await insertSql('resource', resourceKeys, Array.of<any>('资源管理', 'ResourceMgt', 'MENU', parent.id, '/system/resource', null, 'i-fe:list', '/src/views/system/resource/index.vue', '', null, null, null, 1, 1, 0, 1729218241039, 1729218241039));
-  await insertSql('resource', resourceKeys, Array.of<any>('角色管理', 'RoleMgt', 'MENU', parent.id, '/system/role', null, 'i-fe:user-check', '/src/views/system/role/index.vue', '', null, null, null, 1, 1, 1, 1729218286003, 1729218286003));
-  parent = await insertSql('resource', resourceKeys, Array.of<any>('用户管理', 'UserMgt', 'MENU', parent.id, '/system/user', null, 'i-fe:users', '/src/views/system/user/index.vue', '', null, null, null, 1, 1, 2, 1729218355262, 1729218355262), 0);
-  // 插入个人资料菜单
-  await insertSql('resource', resourceKeys, Array.of<any>('个人资料', 'UserProfile', 'MENU', null, '/profile', null, 'i-fe:user', '/src/views/profile/index.vue', '', null, null, null, 0, 1, 3, 1729218405995, 1729218405995));
+  let parent = await insertResource({
+    name: '基础功能',
+    code: 'Base',
+    type: 'MENU',
+    path: null,
+    redirect: null,
+    icon: 'i-fe:grid',
+    component: null,
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 0,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
 
-  // 插入admin角色资源权限
-  const casinbKeys = ["ptype", "v0", "v1", "v2", "v3", "v4", "v5"];
-  const acessCodes= [
-    'Base',
-    'Icon', 
-    'BaseComponents', 
-    'Unocss', 
-    'KeepAlive', 
-    'TestModal', 
-    'Demo', 
-    'ImgUpload', 
-    'CrudDemo', 
-    'DemoRichText', 
-    'Data', 
-    'DataDict', 
-    'DataFile', 
-    'SysMgt', 
-    'ResourceMgt', 
-    'RoleMgt', 
-    'UserMgt', 
-    'UserProfile',
-    'demo:crud:add', 
-    'demo:crud:remove', 
-    'demo:crud:edit', 
-    'demo:crud:view',
+  await insertResource({
+    name: '图标 Icon',
+    code: 'Icon',
+    type: 'MENU',
+    parent: {
+      connect: { id: parent.id },
+    },
+    path: '/base/icon',
+    redirect: null,
+    icon: 'i-fe:feather',
+    component: '/src/views/base/unocss-icon.vue',
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 0,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  await insertResource({
+    name: '基础组件',
+    code: 'BaseComponents',
+    type: 'MENU',
+    parent: {
+      connect: { id: parent.id },
+    },
+    path: '/base/components',
+    redirect: null,
+    icon: 'i-me:awesome',
+    component: '/src/views/base/index.vue',
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 1,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  await insertResource({
+    name: 'Unocss',
+    code: 'Unocss',
+    type: 'MENU',
+    parent: {
+      connect: { id: parent.id },
+    },
+    path: '/base/unocss',
+    redirect: null,
+    icon: 'i-me:awesome',
+    component: '/src/views/base/unocss.vue',
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 2,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  await insertResource({
+    name: 'KeepAlive',
+    code: 'KeepAlive',
+    type: 'MENU',
+    parent: {
+      connect: { id: parent.id },
+    },
+    path: '/base/keep-alive',
+    redirect: null,
+    icon: 'i-me:awesome',
+    component: '/src/views/base/keep-alive.vue',
+    layout: '',
+    keepAlive: true,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 3,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  await insertResource({
+    name: 'MeModal',
+    code: 'TestModal',
+    type: 'MENU',
+    parent: {
+      connect: { id: parent.id },
+    },
+    path: '/testModal',
+    redirect: null,
+    icon: 'i-me:dialog',
+    component: '/src/views/base/test-modal.vue',
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 4,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  // 业务示例Demo
+  parent = await insertResource({
+    name: '业务示例',
+    code: 'Demo',
+    type: 'MENU',
+    path: null,
+    redirect: null,
+    icon: 'i-fe:grid',
+    component: null,
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 1,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  await insertResource({
+    name: '图片上传',
+    code: 'ImgUpload',
+    type: 'MENU',
+    parent: {
+      connect: { id: parent.id },
+    },
+    path: '/demo/upload',
+    redirect: null,
+    icon: 'i-fe:image',
+    component: '/src/views/demo/upload/index.vue',
+    layout: '',
+    keepAlive: true,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 0,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  let crudItem = await insertResource({
+    name: 'CRUD表单',
+    code: 'CrudDemo',
+    type: 'MENU',
+    parent: {
+      connect: { id: parent.id },
+    },
+    path: '/demo/crud',
+    redirect: null,
+    icon: 'i-fe:database',
+    component: '/src/views/demo/crud/index.vue',
+    layout: '',
+    keepAlive: true,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 0,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  await insertResource({
+    name: '富文本',
+    code: 'DemoRichText',
+    type: 'MENU',
+    parent: {
+      connect: { id: parent.id },
+    },
+    path: '/demo/rich-text',
+    redirect: null,
+    icon: 'i-fe:database',
+    component: '/src/views/demo/rich-text/index.vue',
+    layout: '',
+    keepAlive: true,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 0,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  // crud例子-权限按钮
+  await insertResource({
+    name: '添加',
+    code: 'demo:crud:add',
+    type: 'BUTTON',
+    parent: {
+      connect: { id: crudItem.id },
+    },
+    path: null,
+    redirect: null,
+    icon: null,
+    component: null,
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 0,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  await insertResource({
+    name: '删除',
+    code: 'demo:crud:remove',
+    type: 'BUTTON',
+    parent: {
+      connect: { id: crudItem.id },
+    },
+    path: null,
+    redirect: null,
+    icon: null,
+    component: null,
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 0,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  await insertResource({
+    name: '查看',
+    code: 'demo:crud:view',
+    type: 'BUTTON',
+    parent: {
+      connect: { id: crudItem.id },
+    },
+    path: null,
+    redirect: null,
+    icon: null,
+    component: null,
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 0,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  await insertResource({
+    name: '编辑',
+    code: 'demo:crud:edit',
+    type: 'BUTTON',
+    parent: {
+      connect: { id: crudItem.id },
+    },
+    path: null,
+    redirect: null,
+    icon: null,
+    component: null,
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 0,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  // 数据管理
+  parent = await insertResource({
+    name: '数据管理',
+    code: 'Data',
+    type: 'MENU',
+    path: null,
+    redirect: null,
+    icon: 'i-fe:grid',
+    component: null,
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 1,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  await insertResource({
+    name: '数据字典',
+    code: 'DataDict',
+    type: 'MENU',
+    parent: {
+      connect: { id: parent.id },
+    },
+    path: '/data/dict',
+    redirect: null,
+    icon: 'i-fe:book',
+    component: '/src/views/data/dict/index.vue',
+    layout: '',
+    keepAlive: true,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 0,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  await insertResource({
+    name: '文件管理',
+    code: 'DataFile',
+    type: 'MENU',
+    parent: {
+      connect: { id: parent.id },
+    },
+    path: '/data/file',
+    redirect: null,
+    icon: 'i-fe:file',
+    component: '/src/views/data/file/index.vue',
+    layout: '',
+    keepAlive: true,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 0,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  // 系统管理
+  parent = await insertResource({
+    name: '系统管理',
+    code: 'SysMgt',
+    type: 'MENU',
+    path: null,
+    redirect: null,
+    icon: 'i-fe:grid',
+    component: null,
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 2,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  await insertResource({
+    name: '资源管理',
+    code: 'ResourceMgt',
+    type: 'MENU',
+    parent: {
+      connect: { id: parent.id },
+    },
+    path: '/system/resource',
+    redirect: null,
+    icon: 'i-fe:list',
+    component: '/src/views/system/resource/index.vue',
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 0,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  await insertResource({
+    name: '角色管理',
+    code: 'RoleMgt',
+    type: 'MENU',
+    parent: {
+      connect: { id: parent.id },
+    },
+    path: '/system/role',
+    redirect: null,
+    icon: 'i-fe:user-check',
+    component: '/src/views/system/role/index.vue',
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 1,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  parent = await insertResource({
+    name: '用户管理',
+    code: 'UserMgt',
+    type: 'MENU',
+    parent: {
+      connect: { id: parent.id },
+    },
+    path: '/system/user',
+    redirect: null,
+    icon: 'i-fe:users',
+    component: '/src/views/system/user/index.vue',
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: true,
+    enable: true,
+    order: 2,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  // 插入个人资料菜单
+  await insertResource({
+    name: '个人资料',
+    code: 'UserProfile',
+    type: 'MENU',
+    path: '/profile',
+    redirect: null,
+    icon: 'i-fe:user',
+    component: '/src/views/profile/index.vue',
+    layout: '',
+    keepAlive: null,
+    method: null,
+    description: null,
+    show: false,
+    enable: true,
+    order: 3,
+    createTime: new Date(),
+    updateTime: new Date(),
+  });
+
+  // 插入casbin_rule数据
+  const insertCasbinRule = async (data: Prisma.CasbinRuleCreateInput) => {
+    await prisma.casbinRule.create({
+      data: { ...data },
+    });
+  };
+
+  const accessCodes = [
+    'Base', 'Icon', 'BaseComponents', 'Unocss', 'KeepAlive', 'TestModal',
+    'Demo', 'ImgUpload', 'CrudDemo', 'DemoRichText', 'Data', 'DataDict',
+    'DataFile', 'SysMgt', 'ResourceMgt', 'RoleMgt', 'UserMgt', 'UserProfile',
+    'demo:crud:add', 'demo:crud:remove', 'demo:crud:edit', 'demo:crud:view',
   ];
-  for (const code of acessCodes) {
-    await insertSql('casbin_rule', casinbKeys, Array.of<any>('p', ADMIN_ROLE_NAME, code, 'access', null, null, null));
+
+  for (const code of accessCodes) {
+    await insertCasbinRule({
+      ptype: 'p',
+      v0: ADMIN_ROLE_NAME,
+      v1: code,
+      v2: 'access',
+      v3: null,
+      v4: null,
+      v5: null,
+    });
   }
 
   // 插入文件管理
@@ -166,12 +579,21 @@ const main = async () => {
     },
   });
 
+  console.log('insert Demo.');
+
   // 示例数据
-  const demoKeys = ["name", "gender", "desc", "createTime", "updateTime"];
   for (let i = 0; i < 25; i++) {
-    await insertSql('demo', demoKeys, Array.of<any>('例子' + i, 1, '这是一个例子', 1729218405995, 1729218405995));
+    await prisma.demo.create({
+      data: {
+        name: '例子' + i,
+        gender: 1,
+        desc: '这是一个例子',
+        createTime: new Date(),
+        updateTime: new Date(),
+      }
+    });
   }
- 
+
   console.log('Seeding finished.');
 };
 main()
